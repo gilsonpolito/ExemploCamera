6fddcbcc6b63c7f883e342b911aec42a4ee345cc
gilsonpolito <gilsonpolito@gmail.com>
2018-08-31 21:30:36 -0300

    * ExemploCamera.Android.csproj: Funcionalidades corretas para IPhone
    * README.md:
    * App.xaml.cs:
    * MainPage.xaml:
    * Info.plist:
    * MainPage.xaml.cs:
    * ExemploCamera.csproj:
    * MainActivity.cs:
    * file_paths.xml:
    * AndroidManifest.xml:
    * Resource.Designer.cs:

--- ExemploCamera/App.xaml.cs
+++ ExemploCamera/App.xaml.cs
@@ -11,7 +11,7 @@ namespace ExemploCamera
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- ExemploCamera/MainPage.xaml.cs
+++ ExemploCamera/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace ExemploCamera
 {
@@ -12,6 +14,93 @@ namespace ExemploCamera
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void TirarFoto(object sender, EventArgs args)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            {
+                await DisplayAlert("Ops", "Nenhuma camera detectada", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Demo"
+                });
+
+            if (file == null)
+                return;
+
+            exibirImagem(file);
+        }
+
+        private void exibirImagem(MediaFile file)
+        {
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void EscolherFoto(object sender, EventArgs args)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Ops", "Galeria de fotos não suportada", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if (file == null)
+                return;
+            
+            exibirImagem(file);
+        }
+
+        private async void GravarVideo(object sender, EventArgs args)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsTakeVideoSupported || !CrossMedia.Current.IsCameraAvailable)
+            {
+                await DisplayAlert("Ops", "Nenhuma camera detectada", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakeVideoAsync(
+                new StoreVideoOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Movies",
+                    Quality = VideoQuality.Medium
+                });
+
+            if (file == null)
+                return;
+
+            exibirImagem(file);
+        }
+
+        private async void EscolherVideo(object sender, EventArgs args)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsPickVideoSupported)
+            {
+                await DisplayAlert("Ops", "Galeria de videos não suportada", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickVideoAsync();
+            if (file == null)
+                return;
+
+            exibirImagem(file);
         }
     }
 }
